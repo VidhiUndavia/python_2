6177da137f086d577ad37e75db45f7f8cf0bf349
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-07-20 17:54:33 +0530

    Create arithmaticop.py

---
+++ arithmaticop.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter first number = "))
+num2=int(input("Enter second number = "))
+
+add=num1+num2
+print("add = ",add)
+subs=num1-num2
+print("substraction = ",subs)
+mul=num1*num2
+print("multiplication = ",mul)
+answer1=num1/num2
+print("Division = ",answer1)
+answer2=num1//num2
+print("Floor division answer = ",answer2)
+print("5 ** 3 = ",5**3) #explonent
+
+
