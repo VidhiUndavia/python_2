df458f6b7d9917e3547633a79df2cf6e3d73056c
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-07-23 18:16:45 +0530

    Add files via upload

---
+++ if_else1.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first number = "))
+num2=int(input("Enter the second number = "))
+
+if num1>num2:
+    print("Num1 is grater then num2.")
+else:
+    print("Num2 is grater then num1.")
+print("bye")
---
+++ if_else2.py
@@ -0,0 +1,7 @@
+# write a program which take one number from the user and check given number is odd or even.
+num=int(input("Enter the number = "))
+
+if (num%2)==0:
+    print("Number is even")
+else:
+    print("Number is odd")
