56157658ab3589303158347be338cc67d1dfde6c
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-07 18:47:00 +0530

    Add files via upload

---
+++ for1.py
@@ -0,0 +1,14 @@
+fruit=["mango","graps","orange","apple"]
+
+for item in fruit:
+    print(item)
+
+for x in range(10):
+    print(x)
+
+print("-----------------------------------")
+for x in range(1,11):
+    print(x)
+
+for letter in "The easy learn academy":
+    print(letter)
