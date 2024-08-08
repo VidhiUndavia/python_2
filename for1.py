c35e9ae30abde28bc52509c89088d9f5b80f2f72
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-08 18:38:31 +0530

    Add files via upload

--- for1.py
+++ for1.py
@@ -11,4 +11,11 @@ for x in range(1,11):
     print(x)
 
 for letter in "The easy learn academy":
-    print(letter)
+    print(letter,end=' ')
+print()
+for item in reversed(fruit):
+    print(item,end=' ')
+
+print()
+for x in range(10,0,-1):
+    print(x)
