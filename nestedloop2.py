08789599ac3df4917d64cf6c8903934b8d626612
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-09 18:25:31 +0530

    Add files via upload

---
+++ nestedloop2.py
@@ -0,0 +1,13 @@
+# for x in range(1,6):
+#     for y in range(1,x+1):
+#         print(y,end=' ')
+#     print()
+
+i=1
+while i<=5:
+    j=1
+    while j<=i:
+        print(j,end=' ')
+        j+=1
+    print()
+    i+=1
---
+++ nestedloop3.py
@@ -0,0 +1,6 @@
+i=5
+while i>0:
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+    i-=1
