444c601c44541397518ba37ee4542f936b64ea4f
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-10 18:23:13 +0530

    Add files via upload

---
+++ func1.py
@@ -0,0 +1,18 @@
+def getSquare(num):
+    square=num*num
+    return square   
+
+num=int(input("Enter the number  = "))
+
+ans=getSquare(num)
+print("Square = ",ans)
+
+num=int(input("Enter the number  = "))
+
+ans=getSquare(num)
+print("Square = ",ans)
+
+num=int(input("Enter the number  = "))
+
+ans=getSquare(num)
+print("Square = ",ans)
