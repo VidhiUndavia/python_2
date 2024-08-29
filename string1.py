0725ee0e454aa0f713b3279ea3cd5930fc56bbdb
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-29 18:24:58 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,20 @@
+str="The easy learn academy"
+print(str)
+
+print(str.upper())
+print(str.lower())
+str1="PUJA"
+print(str1.isupper())
+print(str.title())
+print(str.istitle())
+print(str.islower())
+
+str="hello123"
+print(str.isalnum())
+print(str1.isalpha())
+# str="123"
+print(str.isnumeric())
+str="  hi  "
+print(str.isspace())
+str="The easy learn academy"
+print(len(str))
