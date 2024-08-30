366aba45d2db294ff91c706c632339c5ebaa4f81
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-30 18:32:13 +0530

    Add files via upload

--- string1.py
+++ string1.py
@@ -18,3 +18,27 @@ str="  hi  "
 print(str.isspace())
 str="The easy learn academy"
 print(len(str))
+fruit1=["strawberry","pineapple","pineapple","orange"]
+s=" "
+
+print(s.join(fruit1))
+
+python={"name":"Python","price":4500,"duration":90}
+print(s.join(python))
+
+touple2=("99","MCA","99.99","gurukul")
+print(s.join(touple2))
+
+animal={"Dog","Cow","Cat","ret"}
+print(s.join(animal))
+
+sentance="Good morning, this morning is full of sunlight and happiness, have a great morning"
+print("Original = ",sentance)
+print("New = ",sentance.replace("morning","night"))
+
+print("New = ",sentance.replace("morning","night",2))
+
+s=','
+name="Theeasylearnacademy"
+list1=name.split(s)
+print(list1)
